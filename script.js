30ae44dabec7808eea766ccef7d0de125913faeb
prakhar-174 <prakhartiwari174@gmail.com>
2025-03-06 22:03:08 +0530

    confetti Effect

--- script.js
+++ script.js
@@ -73,3 +73,11 @@ document.getElementById('closeCard').addEventListener('click', () => {
 
 // Initialize
 window.onload = createElements;
+
+document.getElementById('confettiButton').addEventListener('click', () => {
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
+});
